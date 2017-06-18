c3495c90cca5b669ae95db10530548f988ba902c
Shahak <yaakov@funkey.mobi>
2017-06-18 08:20:42 +0300

    [ADD] Sample activity

---
+++ app/src/main/java/il/co/yshahak/ivricalendar/calendar/EventsInterface.java
@@ -0,0 +1,15 @@
+package il.co.yshahak.ivricalendar.calendar;
+
+/**
+ * Created by Yaakov Shahak
+ * on 18/06/17.
+ */
+
+public interface EventsInterface {
+
+    void addEvent();
+
+    void deleteEvent(int id);
+
+    void updateEvevnt();
+}
